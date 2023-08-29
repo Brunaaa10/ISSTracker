cbf096180c61ef9214c50c9835b05382ba4de3f8
brunaaa10 <brunaleticiadiniz1@gmail.com>
2023-08-29 20:54:48 -0300

    Média do diâmetro dos meteoros

--- Screens/Meteors.js
+++ Screens/Meteors.js
@@ -11,28 +11,52 @@ export default class Meteors extends React.Component {
 
     }
 
-    componentDidMount(){
-       this.getMeteors()
+    componentDidMount() {
+        this.getMeteors()
     }
 
-    getMeteors =()=> {
-        axios 
-        .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=UEG79o7VuN1AvC2nm6N7ze0clLmLOUaAHrKej06i")
-        .then(response => {
-            this.setState({ meteors: response.data.near_earth_objects }),
-            console.log(Object.keys(this.state.meteors))
-        })
-        .catch(error => {
-            Alert.alert(error.message)
-        })
+    getMeteors = () => {
+        axios
+            .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=ogwqFuEG53qCmM6W7rmzxGIRauhzofIuoVi9XOP4")
+            .then(response => {
+                this.setState({ meteors: response.data.near_earth_objects }),
+                    console.log(Object.keys(this.state.meteors))
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
     }
-    
+
 
     render() {
-        return (
-            <View>
-                <Text>Tela de Meteoros</Text>
-            </View>
-        )
+        if (Object.keys(this.state.meteors).length == 0) {
+            return(
+                <View styles = {{flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Text>Carregando..</Text>
+                </View>
+            )
+
+        } else {
+            var meteorArray = Object.keys(this.state.meteors).map(meteorDate => {
+                return (
+                    this.state.meteors(meteorDate)
+                )
+            })
+            var meteors = [].concat.apply([], meteorArray)
+
+            meteors.forEach(meteor => {
+                var diameter = (meteor.estimated_diameter.kilometers.estimated_diameter_min +
+                    meteor.estimated_diameter.kilometers.estimated_diameter_max) / 2
+
+                var threatScore = (diameter / meteor.close_approach_data[0].miss_distance.kilometers) * 1000000000
+                meteor.threat_score = threatScore
+            });
+
+            return (
+                <View>
+                    <Text>Tela de Meteoros</Text>
+                </View>
+            )
+        }
     }
 }
